4ddbf307e671d0297e1987c484d5ca769cd61038
ZHANG-Qidi <grunt4991@gmail.com>
2026-05-23 21:27:37 +0900

    启用 MCPWM 生成中心对称 PWM

--- components/ArduinoCore-avr-1.8.7/cores/arduino/Arduino_HAL.cpp
+++ components/ArduinoCore-avr-1.8.7/cores/arduino/Arduino_HAL.cpp
@@ -1,9 +1,12 @@
 #include "Arduino_HAL.h"
 
+#include <stdint.h>
+
 #include "Arduino.h"
 #include "Arduino_interface.h"
 #include "driver/gpio.h"
 #include "driver/ledc.h"
+#include "driver/mcpwm_prelude.h"
 #include "esp_rom_sys.h"
 #include "esp_timer.h"
 #include "freertos/FreeRTOS.h"
@@ -67,6 +70,75 @@ void pinMode(uint8_t pin, uint8_t mode) {
     gpio_config(&cfg);
 }
 
+#if CONFIG_SOC_MCPWM_SUPPORTED
+
+#define FOC_MCPWM_TIMER_RESOLUTION_HZ (40000000)
+#define FOC_MCPWM_PERIOD (1000)
+
+static mcpwm_cmpr_handle_t comparators[3];
+
+static void analogWriteInitMcpwm(uint8_t pin_u, uint8_t pin_v, uint8_t pin_w) {
+    mcpwm_timer_handle_t timer;
+    mcpwm_timer_config_t timer_config = {};
+    timer_config.clk_src = MCPWM_TIMER_CLK_SRC_DEFAULT;
+    timer_config.group_id = 0;
+    timer_config.resolution_hz = FOC_MCPWM_TIMER_RESOLUTION_HZ;
+    timer_config.period_ticks = FOC_MCPWM_PERIOD;
+    timer_config.count_mode = MCPWM_TIMER_COUNT_MODE_UP_DOWN;
+    ESP_ERROR_CHECK(mcpwm_new_timer(&timer_config, &timer));
+
+    mcpwm_oper_handle_t operators[3];
+    mcpwm_operator_config_t operator_config = {};
+    operator_config.group_id = 0;  // operator should be in the same group of the above timers
+    for (int i = 0; i < 3; i++) {
+        ESP_ERROR_CHECK(mcpwm_new_operator(&operator_config, &operators[i]));
+        ESP_ERROR_CHECK(mcpwm_operator_connect_timer(operators[i], timer));
+    }
+
+    mcpwm_comparator_config_t compare_config = {};
+    compare_config.flags.update_cmp_on_tez = true;
+    for (int i = 0; i < 3; i++) {
+        ESP_ERROR_CHECK(mcpwm_new_comparator(operators[i], &compare_config, &comparators[i]));
+        ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(comparators[i], 0));
+    }
+
+    mcpwm_gen_handle_t generators[3];
+    int gen_gpios[3] = {pin_u, pin_v, pin_w};
+    mcpwm_generator_config_t gen_config = {};
+    for (int i = 0; i < 3; i++) {
+        gen_config.gen_gpio_num = gen_gpios[i];
+        ESP_ERROR_CHECK(mcpwm_new_generator(operators[i], &gen_config, &generators[i]));
+    }
+
+    for (int i = 0; i < 3; i++) {
+        ESP_ERROR_CHECK(mcpwm_generator_set_action_on_compare_event(generators[i], MCPWM_GEN_COMPARE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, comparators[i], MCPWM_GEN_ACTION_LOW)));
+        ESP_ERROR_CHECK(mcpwm_generator_set_action_on_compare_event(generators[i], MCPWM_GEN_COMPARE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_DOWN, comparators[i], MCPWM_GEN_ACTION_HIGH)));
+    }
+
+    ESP_ERROR_CHECK(mcpwm_timer_enable(timer));
+    ESP_ERROR_CHECK(mcpwm_timer_start_stop(timer, MCPWM_TIMER_START_NO_STOP));
+}
+
+void analogWrite(uint8_t pin, int value) {
+    if (value < 0) value = 0;
+    if (value > 255) value = 255;
+    uint8_t index = pin % 3;
+    uint32_t level = value / 255.0f * (float)(FOC_MCPWM_PERIOD) / 2.0f;
+    // Regular uvw data to (0 ~ (EXAMPLE_FOC_MCPWM_PERIOD/2))
+    ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(comparators[index], level));
+}
+
+// function setting the high pwm frequency to the supplied pins
+// - BLDC motor - 3PWM setting
+// - hardware speciffic
+// in generic case dont do anything
+void _configure3PWM(long pwm_frequency, const int pinA, const int pinB, const int pinC) {
+    (void)(pwm_frequency);
+    analogWriteInitMcpwm(pinA, pinB, pinC);
+}
+
+#else
+
 void analogWrite(uint8_t pin, int value) {
     if (value < 0) value = 0;
     if (value > 255) value = 255;
@@ -109,3 +181,4 @@ void _configure3PWM(long pwm_frequency, const int pinA, const int pinB, const in
     analogWriteInit(pinB);
     analogWriteInit(pinC);
 }
+#endif
